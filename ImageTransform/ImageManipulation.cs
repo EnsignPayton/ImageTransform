49a7d7d92025c5e340f38c8ce82c03f4153534e6
Logan Payton <Logan.Payton@javs.com>
2018-06-04 17:28:57 -0400

    Make it more better

---
+++ ImageTransform/ImageManipulation.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+
+namespace ImageTransform
+{
+    /// <summary>
+    /// Batch manipulates images
+    /// </summary>
+    internal class ImageManipulation
+    {
+        public event EventHandler<MadeTransparentEventArgs> MadeTransparent;
+        public event EventHandler<ImageResizedEventArgs> ImageResized;
+
+        public string WorkingPath { get; set; }
+
+        /// <summary>
+        /// Makes transparent PNGs from all BMP images in <see cref="WorkingPath"/>.
+        /// </summary>
+        /// <param name="transparentColor">Color to make transparent</param>
+        public void MakeTransparent(Color transparentColor)
+        {
+            foreach (var file in Directory.EnumerateFiles(WorkingPath, "*.bmp"))
+            {
+                var newName = Path.ChangeExtension(file, "png");
+                using (var image = Image.FromFile(file))
+                using (var bitmap = new Bitmap(image))
+                {
+                    bitmap.MakeTransparent(transparentColor);
+                    bitmap.Save(newName, ImageFormat.Png);
+                    MadeTransparent?.Invoke(this, new MadeTransparentEventArgs(file, newName));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Makes transparent PNGs from all BMP images in <see cref="WorkingPath"/>.
+        /// </summary>
+        /// <param name="hexValue">RGB Hex of color to make transparent</param>
+        public void MakeTransparent(string hexValue)
+        {
+            var transparentColor = Color.FromArgb(int.Parse(hexValue.Replace("#", ""), NumberStyles.HexNumber));
+
+            MakeTransparent(transparentColor);
+        }
+
+        /// <summary>
+        /// Changes the dimensions of all image in <see cref="WorkingPath"/> with <see cref="oldX"/> x <see cref="oldY"/> to <see cref="newX"/> x <see cref="newY"/>.
+        /// </summary>
+        /// <param name="oldX">Old width</param>
+        /// <param name="oldY">Old height</param>
+        /// <param name="newX">New width</param>
+        /// <param name="newY">New height</param>
+        public void Resize(int oldX, int oldY, int newX, int newY)
+        {
+            foreach (var file in Directory.EnumerateFiles(WorkingPath, "*.png", SearchOption.AllDirectories))
+            {
+                string tmpFile = file + ".tmp";
+                using (var image = Image.FromFile(file))
+                {
+                    if (image.Width != oldX || image.Height != oldY) continue;
+
+                    using (var bitmap = ResizeImage(image, newX, newY))
+                    {
+                        bitmap.Save(tmpFile, ImageFormat.Png);
+                        ImageResized?.Invoke(this, new ImageResizedEventArgs(file, oldX, oldY, newX, newY));
+                    }
+                }
+
+                File.Delete(file);
+                File.Move(tmpFile, file);
+            }
+        }
+
+        /// <summary>
+        /// Changes the dimensions of all image in <see cref="WorkingPath"/> with <see cref="oldDim"/> to <see cref="newDim"/>.
+        /// </summary>
+        /// <param name="oldDim">Old dimensions (XXxYY)</param>
+        /// <param name="newDim">New dimensions (XXxYY)</param>
+        public void Resize(string oldDim, string newDim)
+        {
+            var oldDims = oldDim.Split('x');
+            var newDims = newDim.Split('x');
+            var oldX = int.Parse(oldDims[0]);
+            var oldY = int.Parse(oldDims[1]);
+            var newX = int.Parse(newDims[0]);
+            var newY = int.Parse(newDims[1]);
+
+            Resize(oldX, oldY, newX, newY);
+        }
+
+        // Credit where credit is due: https://stackoverflow.com/a/24199315
+        private static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(image))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.NearestNeighbor; // Original: HighQualityBicubic
+                graphics.SmoothingMode = SmoothingMode.None; // Original: HighQuality
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var imageAttr = new ImageAttributes())
+                {
+                    imageAttr.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width,
+                        image.Height, GraphicsUnit.Pixel, imageAttr);
+                }
+            }
+
+            return destImage;
+        }
+    }
+
+    internal class MadeTransparentEventArgs : EventArgs
+    {
+        public string OldImageName { get; }
+        public string NewImageName { get; }
+
+        public MadeTransparentEventArgs(string oldImageName, string newImageName)
+        {
+            OldImageName = oldImageName;
+            NewImageName = newImageName;
+        }
+    }
+
+    internal class ImageResizedEventArgs : EventArgs
+    {
+        public string ImageName { get; }
+        public int OldX { get; }
+        public int OldY { get; }
+        public int NewX { get; }
+        public int NewY { get; }
+        public string OldDims => OldX + "x" + OldY;
+        public string NewDims => NewX + "x" + NewY;
+
+        public ImageResizedEventArgs(string imageName, int oldX, int oldY, int newX, int newY)
+        {
+            ImageName = imageName;
+            OldX = oldX;
+            OldY = oldY;
+            NewX = newX;
+            NewY = newY;
+        }
+    }
+}
--- ImageTransform/Program.cs
+++ ImageTransform/Program.cs
@@ -1,38 +1,33 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Globalization;
 using System.IO;
 
 namespace ImageTransform
 {
     internal class Program
     {
-        private const string ErrUsage = "Usage: ImageTransform.exe <folderPath> <hexColor>";
+        private const string ErrUsage = "Usage: ImageTransform.exe <folderPath> <hexColor> OR [<oldDim> <newDim>]";
         private const string ErrArgCount = "Invalid argument count.";
         private const string ErrInvalidPath = "Invalid folder path.";
+        private const string ErrInvalidArg = "Invalid argument.";
 
         private static void Main(string[] args)
         {
             try
             {
-                if (args.Length != 2) throw new ArgumentException(ErrArgCount);
+                if (args.Length < 2 || args.Length > 3) throw new ArgumentException(ErrArgCount);
                 if (!Directory.Exists(args[0])) throw new ArgumentException(ErrInvalidPath);
 
-                var folderPath = args[0];
-                var targetColor = Color.FromArgb(int.Parse(args[1].Replace("#", ""), NumberStyles.HexNumber));
-
-                foreach (var file in Directory.EnumerateFiles(folderPath))
+                if (args[1].Contains("#"))
                 {
-                    if (!Path.GetExtension(file).Contains("bmp")) continue;
-                    var newName = Path.ChangeExtension(file, "png");
-                    using (var image = Image.FromFile(file))
-                    using (var bitmap = new Bitmap(image))
-                    {
-                        bitmap.MakeTransparent(targetColor);
-                        bitmap.Save(newName, ImageFormat.Png);
-                        Console.WriteLine(file + " --> " + newName);
-                    }
+                    MakeTransparent(args[0], args[1]);
+                }
+                else if (args[1].Contains("x"))
+                {
+                    ResizeImages(args[0], args[1], args[2]);
+                }
+                else
+                {
+                    throw new ArgumentException(ErrInvalidArg);
                 }
             }
             catch (Exception ex)
@@ -42,6 +37,20 @@ namespace ImageTransform
             }
 
             Console.Read();
+        }
+
+        private static void MakeTransparent(string folderPath, string hexColor)
+        {
+            var im = new ImageManipulation {WorkingPath = folderPath};
+            im.MadeTransparent += (s, e) => Console.WriteLine(e.OldImageName + " --> " + e.NewImageName);
+            im.MakeTransparent(hexColor);
+        }
+
+        private static void ResizeImages(string folderPath, string oldDim, string newDim)
+        {
+            var im = new ImageManipulation {WorkingPath = folderPath};
+            im.ImageResized += (s, e) => Console.WriteLine(e.ImageName + ": " + e.OldDims + " --> " + e.NewDims);
+            im.Resize(oldDim, newDim);
         }
     }
 }
